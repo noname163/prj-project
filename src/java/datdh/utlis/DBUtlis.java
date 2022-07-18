8532054a2363685e1c6e404825ffbf1fb7c96591
DangHuuDat <danghuudat163@gmail.com>
2022-07-18 18:11:29 +0700

    add MySql connect

--- src/java/datdh/utlis/DBUtlis.java
+++ src/java/datdh/utlis/DBUtlis.java
@@ -13,6 +13,7 @@ import java.sql.SQLException;
  * @author ADMIN
  */
 public class DBUtlis {
+
     private final String serverName = "SE150011\\DATDHSE150011";
     private final String dbName = "PlantShop";
     private final String portNumber = "1433";
@@ -21,16 +22,31 @@ public class DBUtlis {
     private final String pass = "dat163163";
 
     public Connection getConnection() throws ClassNotFoundException, SQLException {
-        String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\"
-                + instance + ";databaseName=" + dbName;
-        if (instance == null || instance.trim().isEmpty()) {
-            url = "jdbc:sqlserver://" + serverName + ":" + portNumber
-                    + ";databaseName=" + dbName;
+//        String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\"
+//                + instance + ";databaseName=" + dbName;
+//        if (instance == null || instance.trim().isEmpty()) {
+//            url = "jdbc:sqlserver://" + serverName + ":" + portNumber
+//                    + ";databaseName=" + dbName;
+//        }
+//        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//        return DriverManager.getConnection(url, userName, pass);
+        String url = "jdbc:mysql://localhost:3306/PlantShop";
+        String username = "root";
+        String password = "root";
+
+        System.out.println("Connecting database...");
+
+        try ( 
+            Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Database connected!");
+            Class.forName("com.mysql.jdbc.Driver");
+            return connection;
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
         }
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        return DriverManager.getConnection(url, userName, pass);
+
     }
-    
+
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         DBUtlis a = new DBUtlis();
         System.out.println(a.getConnection());
